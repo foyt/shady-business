91903fc51a67955603642818d42c4f20ec2d621a
Antti Leppä <antti.leppa@foyt.fi>
2016-04-18 20:45:43 +0300

    ** Changed index to use class

--- index/index.js
+++ index/index.js
@@ -1,114 +1,139 @@
 (function() {
   'use strict';
+  
   var config = require('../config.json');
   var _ = require("underscore");
   var async = require('async');
-
   var elasticsearch = require('elasticsearch');
-  var client = new elasticsearch.Client({
-    host: config.elasticSearchHost
-  });
-  
-  function indexExists(callback) {
-    client.indices.exists({
-      index: 'shadybussiness'
-    }, callback);
-  }
-  
-  function createIndex(callback) {
-    client.indices.create({
-      index: 'shadybussiness'
-    }, callback);
-  }
-  
-  function updateMapping() {
-    client.indices.putMapping({
-      index: 'shadybussiness',
-      type: 'place',
-      body : {
-        properties : {
-          location: {
-            type : "geo_point",
-            "fielddata" : {
-              "precision" : "1m"
-            }
-          }
-        }
-      }
-    });
-  }
-  
-  function prepareIndex() {
-    indexExists(function (err, exists) {
-      if (err) {
-        console.error(err);
-      } else {
-        if (!exists) {
-          createIndex(function () {
-            updateMapping();
-          });
-        } else {
-          updateMapping();
-        }
-      }
-    });
-  }
-  
-  function indexPlaces(places, mainCallback) {
-    var calls = _.map(places, function (place) {
-      return function (callback) {
-        indexPlace(place, callback);
-      };
-    });
+
+  var Index = class {
     
-    async.parallel(calls, mainCallback);
-  }
-  
-  function indexPlace(place, callback) {
-    client.index({
-      index: 'shadybussiness',
-      type: 'place',
-      id: place.id,
-      body: {
-        name: place.name,
-        description: place.description,
-        tags: place.tags,
-        location: [place.location.latitude, place.location.longitude]
-      }
-    }, function (err, response) {
-      callback(err, response);
-    });
-  }
-  
-  function searchPlacesNear(latitude, longitude, callback) {
-    client.search({
-      index: 'shadybussiness',
-      type: 'place',
-      body: {
-        query: {
-          filtered: {
-            filter: {
-              geo_distance: {
-                "distance": '10km',
-                "location": [latitude, longitude]
+    constructor() {
+      this._client = new elasticsearch.Client({
+        host: config.elasticSearchHost
+      });
+      
+      this._prepareIndex();
+    }
+    
+    _indexExists (callback) {
+      this._client.indices.exists({
+        index: 'shadybussiness'
+      }, callback);
+    } 
+    
+    _createIndex(callback) {
+      this._client.indices.create({
+        index: 'shadybussiness'
+      }, callback);
+    }
+    
+    _updateMapping() {
+      this._client.indices.putMapping({
+        index: 'shadybussiness',
+        type: 'place',
+        body : {
+          properties : {
+            location: {
+              type : "geo_point",
+              "fielddata" : {
+                "precision" : "1m"
               }
             }
           }
         }
-      }
-    }).then(function (response) {
-      callback(null, response);
-    }).catch(function (error) {
-      callback(error);
-    });
+      });
+    }
+    
+    _prepareIndex() {
+      this._indexExists(function (err, exists) {
+        if (err) {
+          console.error(err);
+        } else {
+          if (!exists) {
+            this._createIndex(function () {
+              this._updateMapping();
+            }.bind(this));
+          } else {
+            this._updateMapping();
+          }
+        }
+      }.bind(this));
+    }
+    
+    indexPlaces(places, mainCallback) {
+      var calls = _.map(places, function (place) {
+        return function (callback) {
+          this.indexPlace(place, callback);
+        }.bind(this);
+      }.bind(this));
+      
+      async.parallel(calls, mainCallback);
+    }
+    
+    indexPlace(place, callback) {
+      this._client.index({
+        index: 'shadybussiness',
+        type: 'place',
+        id: place.id,
+        body: {
+          name: place.name,
+          description: place.description,
+          tags: place.tags,
+          location: [place.location.latitude, place.location.longitude]
+        }
+      }, function (err, response) {
+        callback(err, response);
+      });
+    }
+    
+    searchPlacesNear(latitude, longitude, callback) {
+      this._client.search({
+        index: 'shadybussiness',
+        type: 'place',
+        body: {
+          query: {
+            filtered: {
+              filter: {
+                geo_distance: {
+                  "distance": '10km',
+                  "location": [latitude, longitude]
+                }
+              }
+            }
+          }
+        }
+      }).then(function (response) {
+        callback(null, response);
+      }).catch(function (error) {
+        callback(error);
+      });
+    }
+    
+    searchPlaces(topLeft, bottomRight, callback) {
+      this._client.search({
+        index: 'shadybussiness',
+        type: 'place',
+        body: {
+          query: {
+            filtered: {
+              filter: {
+                geo_bounding_box: {
+                  "top_left" : topLeft,
+                  "bottom_right" : bottomRight
+                }
+              }
+            }
+          }
+        }
+      }).then(function (response) {
+        callback(null, response);
+      }).catch(function (error) {
+        callback(error);
+      });
+    }
   }
   
-  prepareIndex();
-  
-  module.exports = {
-    indexPlace: indexPlace,
-    indexPlaces: indexPlaces,
-    searchPlacesNear: searchPlacesNear
-  };
+  module.exports = new Index();
   
 }).call(this);
